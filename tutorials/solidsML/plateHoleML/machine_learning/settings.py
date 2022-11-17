64853b8fece29e8ed2338da8f12276dca62d2689
Scott Levie <scott.levie@ucdconnect.ie>
2022-11-17 11:13:51 +0000

    Update plateHoleML tutorial

--- tutorials/solidsML/plateHoleML/machine_learning/settings.py
+++ tutorials/solidsML/plateHoleML/machine_learning/settings.py
@@ -8,7 +8,7 @@ maxtimestep = 10
 answer_epochs = 500
  
 # Train or load model ("t" = train, "l" = load)
-answer_trainOrLoadModel = "t"
+answer_trainOrLoadModel = "l"
 
 # Prints training update (slows down training) 0 = Off,  1 = ON
 verbose_setting = 1 
