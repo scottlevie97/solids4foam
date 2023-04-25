4c8817a987f708f74b976bec7e262cbe943b14aa
Scott Levie <scott.levie@ucdconnect.ie>
2023-04-25 11:51:39 -0500

    Add narrow t member with no name

---
+++ tutorials/solidsML/base_cases/narrowTmember/machine_learning/settings.py
@@ -0,0 +1,19 @@
+#  Filepath for training case
+filepath1 = "../coarse_mesh/base/"
+
+trainIterations = 20
+Timestepsize = 1
+maxtimestep = 1
+
+answer_epochs = 500
+ 
+# Train or load model ("t" = train, "l" = load)
+answer_trainOrLoadModel = "t"
+
+# Prints training update (slows down training) 0 = Off,  1 = ON
+verbose_setting = 2
+
+# Print training graph
+trainingGraph = False
+
+randomseed = 1
